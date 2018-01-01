5edb40003bdee278c92043452a37174b67c54cfa
SanketGuru <sanketguru33@gmail.com>
2018-01-01 13:38:12 +0530

    Express Login Api

---
+++ todoWebApp/data_repository/datarepo.js
@@ -0,0 +1,118 @@
+'use strict';
+//How to spese fy momgous schema
+//http://mongoosejs.com/docs/schematypes.html
+
+//region Imports
+
+var config=require('../config')
+var mongoose = require('mongoose');
+//endregion
+console.log('Importing Datarepo module');
+var mongoConnection=config.remoteUrl;
+
+console.log('Connecting on MongoServer :'+mongoConnection);
+//export named module
+//exports.dataRepo = function () {
+    mongoose.connect(mongoConnection);
+
+
+//     var mongo = mongoose.model('Todo', {
+//         text: {
+//             type: String,
+//             default: ''
+//         }
+//     });
+
+
+//     mongo.create({
+//         text: "Tesy",
+//         done: false
+//     }, function (err, todo) {
+//         if (err)
+//             res.send(err);
+
+//         // get and return all the todos after you create another
+//         getTodos(res);
+//  });
+
+// }
+
+//Schema for user table
+var userSchema = new mongoose.Schema( {
+    userName: {
+        type: String,
+        default: ''
+    },
+    password: {
+        type: String,
+        default: ''
+    },
+    accessToken: {
+        type: Array,
+        default: []
+    },
+    userSince: {
+        type: Date,
+        default: Date.now
+    }
+});
+var User = mongoose.model('User',userSchema);
+
+
+
+
+//Schema for user table
+var notesSchema = new mongoose.Schema({
+    ownerId: {
+        type: String,
+        default: ''
+    },
+    title: {
+        type: String,
+        default: ''
+    },
+    text: {
+        type: String,
+        default: ''
+    },
+    type: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 3
+    },
+    status: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 3
+    },
+    sharedWith: {
+        type: Array,
+        default: []
+    },
+    creationDate: {
+        type: Date,
+        default: Date.now
+    },
+    lastEdited: {
+        type: Date,
+        default: Date.now
+    },
+    doByDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+var Notes = mongoose.model('Notes', notesSchema);
+
+
+// exports.dataRepo.UserCollection=User;
+
+// exports.dataRepo.NotesCollection=Notes;
+
+exports.dataRepo={
+    dataRepo:"Datarepo is ok",
+    Usercollection: User,
+    NotesCollection : Notes
+}
---
+++ todoWebApp/routes/index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+const repo = require('../data_repository/datarepo');
+var router = express.Router();
+
+var response = {
+  status: true,
+  message: "Login",
+  payload: {}
+}
+
+var apiHead = "/api/v1";
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/*login Api*/
+router.post(apiHead + '/login', function (req, res, next) {
+  var userCon = repo.dataRepo.Usercollection;
+  try {
+    var ss = userCon.findOne({ 'userName': req.body.userName }, 'userName password', function (err, data) {
+      if (err) {
+        response.status = false;
+        response.message = "Some thing went wrong" + err;
+        response.payload = data;
+        res.json(response);
+        return 400;
+      }
+      else {
+        if (data) {
+        
+          if(data.password==req.body.password){
+            response.message = "Go in app ";
+            response.payload = data;
+            res.json(response);
+            return data;
+          }else{
+            response.status = false;
+            response.message = "Invalid ";
+            response.payload = {};
+            res.json(response);
+            return ;
+          }
+       
+        }
+        else {
+          response.status = false;
+          response.message = "No such user found";
+          response.payload = data;
+          res.json(response);
+          return data;
+        }
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    response.status = false;
+    response.message = "Some thing went wrong" + e;
+    response.payload = data;
+    res.json(response);
+  }
+});
+module.exports = router;
