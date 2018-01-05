ab88ab4b3da959474dd256d34e14e9267c10c086
SanketGuru <sanketguru33@gmail.com>
2018-01-05 17:34:20 +0530

    New model created

--- NotesApp/domain/src/main/java/com/sanketguru/domain/todos/TodoModel.kt
+++ NotesApp/domain/src/main/java/com/sanketguru/domain/todos/TodoModel.kt
@@ -3,5 +3,4 @@ package com.sanketguru.domain.todos
 /**
  * Created by Sanket Gurav on 1/5/2018.
  */
-class TodoModel {
-}
+data class TodoModel (var title : String, var text : String,var  id : String)
