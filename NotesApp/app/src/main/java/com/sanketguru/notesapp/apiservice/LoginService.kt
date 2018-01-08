b082b42d4850120b5b042f1243eff37e2f9a11d7
rggupta72 <rggupta72@gmail.com>
2018-01-08 12:56:48 +0530

    my nwe branch

--- NotesApp/app/src/main/java/com/sanketguru/notesapp/apiservice/LoginService.kt
+++ NotesApp/app/src/main/java/com/sanketguru/notesapp/apiservice/LoginService.kt
@@ -1,12 +1,9 @@
 package com.sanketguru.notesapp.apiservice
 
-import com.sanketguru.notesapp.models.CreateNote
 import com.sanketguru.notesapp.models.User
-
 import io.reactivex.Observable
 import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.PUT
 
 /**
  * Created by Sanket Gurav on 1/2/2018.
@@ -15,6 +12,9 @@ import retrofit2.http.PUT
 interface LoginService {
     @POST("login")
     fun login(@Body user: User): Observable<ApiResponse<User>>
+
+    @POST("register")
+    fun register(@Body register: User): Observable<ApiResponse<User>>
 
 
 }
