1e2881b2b56f46375c058c3e45d2b9feabd0b343
SanketGuru <sanketguru33@gmail.com>
2018-01-09 12:27:04 +0530

    Modules Made

--- NotesApp/data/src/main/java/com/sanketguru/data/mapper/UserMapper.kt
+++ NotesApp/data/src/main/java/com/sanketguru/data/mapper/UserMapper.kt
@@ -7,8 +7,8 @@ import com.sanketguru.notesapp.models.User as  User
  * Created by Sanket Gurav on 1/8/2018.
  */
 class UserMapper {
-   public fun transform(user : User)  = com.sanketguru.domain.module.User(user.id,user.userName,user.password,user?.accesstoken?: "")
+   public fun transform(user : User)  = UserModel(user.id,user.userName,user.password,user?.accesstoken?: "")
 
-    public  fun transform(user : com.sanketguru.domain.module.User) = User(user?.id,user?.userName,user?.password,user?.accesstoken)
+    public  fun transform(user : UserModel) = User(user?.id,user?.userName,user?.password,user?.accesstoken)
 
 }
