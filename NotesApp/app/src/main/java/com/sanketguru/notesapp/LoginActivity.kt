cee96b26c88a3427aa5396fa5d157d3ec9d7065b
rggupta72 <rggupta72@gmail.com>
2018-01-12 13:04:27 +0530

    Commit after resolving conflict

--- NotesApp/app/src/main/java/com/sanketguru/notesapp/LoginActivity.kt
+++ NotesApp/app/src/main/java/com/sanketguru/notesapp/LoginActivity.kt
@@ -1,36 +1,31 @@
 package com.sanketguru.notesapp
 
-import android.content.Intent
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import com.sanketguru.notesapp.apiservice.AccountDetails
-import com.sanketguru.notesapp.apiservice.RetrofitHelper
-import com.sanketguru.notesapp.models.User
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Consumer
-import io.reactivex.schedulers.Schedulers
+import com.jakewharton.rxbinding2.view.RxView
+import com.sanketguru.domain.module.UserUIErrorModel
+import com.sanketguru.domain.module.UserUIModel
 import kotlinx.android.synthetic.main.login.*
 
 /**
  * Created by Bhavesh on 02-01-2018.
  */
-class Login : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginActivityViewModel.LoginListener {
 
+    var ss= ClickListner()
+    private lateinit var viewModel: LoginActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        buttonRegister.setOnClickListener { view ->
-            val intent = Intent(this@Login, Register::class.java)
-            startActivity(intent)
-            finish()
+        viewModel = ViewModelProviders.of(this).get(LoginActivityViewModel::class.java!!)
 
-        }
-
-        buttonLogin.setOnClickListener { view ->
-            //var usreName=   etUserName.text
-            var user = User()
+      //RxView.clicks(buttonLogin).subscribe(ss.lis)
+        buttonLogin.setOnClickListener { view -> viewModel.login(UserUIModel("", etUserName.text.toString(), etPassword.text.toString(), "")) }
+        //var usreName=   etUserName.text
+/*            var user = UserUIModel()
             with(user) {
                 userName = etUserName.text.toString()
                 password = etPassword.text.toString()
@@ -38,44 +33,32 @@ class Login : AppCompatActivity() {
 
             }
             var retHelper = RetrofitHelper()
-            var loginData = retHelper.webService.login(user)
-            loginData.subscribeOn(Schedulers.io())
+         var loginData=   retHelper.webService.login(user)
+            loginData  .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(Consumer { response ->
-                        Log.v("Say", response.isSuccess.toString())
+                    .subscribe(Consumer {
+                        response ->
+                        Log.v("Say",response.isSuccess.toString())
                         if (response.isSuccess) {
-                            Log.v("Say", response.payload!!.id)
-                            with(AccountDetails) {
-                                id = response.payload!!.id
-                                userName = response.payload!!.userName
-                            }
-
-                            val intent = Intent(this@Login, MainActivity::class.java)
-                            startActivity(intent)
-                            finish()
-                        }
-                    }, Consumer { err -> err.printStackTrace() })
-
-            /*
-
-            { response ->
-                        {
                             Log.v("Say",response.payload!!.id)
-                            if (response.isSuccess) {
-                               Log.v("Say",response.payload!!.id)
-                            // Log.v("Logsay",);
-//                                val intent = Intent(this@Login, MainActivity::class.java)
-//                                startActivity(intent)
-//                                finish()
+                            with(AccountDetails){
+                                id=response.payload!!.id
+                                userName=response.payload!!.userName
                             }
+
+                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
                         }
-                    }, { t -> {} }
+                    }, Consumer { err -> err.printStackTrace()} )*/
+
+
+        // }
+    }
 
     override fun validationCheck(userUIModel: UserUIErrorModel) {
         Log.v("userNameBlank", userUIModel.userName)
         Log.v("userNamePassword", userUIModel.password)
 
-        }
     }
-
 }
