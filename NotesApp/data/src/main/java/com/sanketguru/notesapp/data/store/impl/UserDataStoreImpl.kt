fb5ab2331173708ca85c208eb69fcc3f7f44f1a8
SanketGuru <sanketguru33@gmail.com>
2018-01-18 17:02:28 +0530

    refactoring

--- NotesApp/data/src/main/java/com/sanketguru/notesapp/data/store/impl/UserDataStoreImpl.kt
+++ NotesApp/data/src/main/java/com/sanketguru/notesapp/data/store/impl/UserDataStoreImpl.kt
@@ -1,18 +1,16 @@
 package com.sanketguru.store.impl
 
-import com.sanketguru.notesapp.apiservice.ApiResponse
 import com.sanketguru.notesapp.apiservice.LoginService
 import com.sanketguru.notesapp.models.User
 import com.sanketguru.store.UserDataStore
-import io.reactivex.Observable
 
 /**
  * Created by Sanket Gurav on 1/8/2018.
  */
 
-class UserDataStoreImpl ( var loginService : LoginService ): UserDataStore {
+class UserDataStoreImpl(var loginService: LoginService) : UserDataStore {
 
 
-    override fun login(user : User) = loginService.login(user)
+    override fun login(user: User) = loginService.login(user)
 
 }
