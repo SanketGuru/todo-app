1199a33ca6f4712c8af957c781b64a8970cef422
SanketGuru <sanketguru33@gmail.com>
2018-02-21 17:37:14 +0530

    Kotlin + buildSrc

---
+++ NotesApp/buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,12 @@
+/**
+ * Created by Sanket Gurav on 2/21/2018.
+ */
+object Version {
+    val myVersion = "33"
+    val retrofit = "33"
+}
+
+object Dependencie {
+    val myVersion = "33"
+    val retrofit = "33"
+}
